59952d34f0bf0ce7348b800fce49ab9b1b0c3944
邹昆峰 <49554020+MuXiu1997@users.noreply.github.com>
2019-07-29 11:57:51 +0800

    以redis作为数据库

--- Config/run_server.sh
+++ Config/run_server.sh
@@ -1,6 +1,13 @@
+docker rm -f JBL_redis
+
 docker rm -f JBL_uwsgi_flask
 
 docker rm -f JBL_nginx
+
+docker run -d -p 6379:6379 --name JBL_redis\
+ -v /JBL/Config/redis.conf:/usr/local/etc/redis/redis.conf\
+ -v /JBL/data:/data\
+ redis:5.0.5 redis-server /usr/local/etc/redis/redis.conf
 
 docker run -d -p 5000:5000 --name JBL_uwsgi_flask\
  -v /JBL/Back_end_Flask/run.py:/app/run.py\
@@ -14,4 +21,3 @@ docker run -d -p 80:80 --name JBL_nginx\
  -v /JBL/Config/nginx.conf:/etc/nginx/nginx.conf\
  -v /JBL/images:/app/images\
  nginx:1.16
-
